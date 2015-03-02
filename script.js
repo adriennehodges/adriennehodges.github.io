303f360ec69b1e77afba9477588a8e0c32fd1452
Adrienne Emerick <adrienneemerick@adrienne-emericks-imac.local>
2015-03-02 07:29:26 -0500

    added bison page and change colors

--- script.js
+++ script.js
@@ -1,44 +1,67 @@
 jQuery(document).ready(function($) {
   
+  var altImage = "_mouseenter.png";
+
   $('#logos img').mouseenter(function() {
 
-  	$(this).attr("src", "images/logos_mouseenter.png")
+  	$(this).attr("src", "images/logos" + altImage).mouseleave(function() {
+
+    $(this).attr("src", "images/logos.png")
+
+    });
 
   });
   
-  $('#logos img').mouseleave(function() {
-
-  	$(this).attr("src", "images/logos.png")
-
-  });
   
   $('#web img').mouseenter(function() {
 
-  	$(this).attr("src", "images/web_mouseenter.png")
+  	$(this).attr("src", "images/web" + altImage).mouseleave(function() {
+
+    $(this).attr("src", "images/web.png")
+
+    });
 
   });
   
-  $('#web img').mouseleave(function() {
-
-  	$(this).attr("src", "images/web.png")
-
-  });
 
   $('#bison img').mouseenter(function() {
 
-  	$(this).attr("src", "images/bison_mouseenter.png")
+  	$(this).attr("src", "images/bison" + altImage).mouseleave(function() {
+
+    $(this).attr("src", "images/bison.png")
+
+    });
 
   });
-  
-  $('#bison img').mouseleave(function() {
 
-  	$(this).attr("src", "images/bison.png")
+  $('#limric img').mouseenter(function() {
+
+    $(this).attr("src", "images/limric" + altImage).mouseleave(function() {
+
+    $(this).attr("src", "images/limric.png")
+
+    });
+
+  });
+
+  $('#fhc img').mouseenter(function() {
+
+    $(this).attr("src", "images/fhc" + altImage).mouseleave(function() {
+
+    $(this).attr("src", "images/fhc.png")
+
+    });
+
+  });
+
+  $('#wedding img').mouseenter(function() {
+
+    $(this).attr("src", "images/wedding" + altImage).mouseleave(function() {
+
+    $(this).attr("src", "images/wedding.png")
+
+    });
 
   });
 
 });
-
-
-/* $( "img" ).attr( "src", function() {
-  return "/resources/" + this.title;
-}); */
